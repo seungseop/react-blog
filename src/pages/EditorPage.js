f43140c4d62aa9595fe9ed4ab9b06b1784b2b18f
Seop <seungseop@users.noreply.github.com>
2019-02-28 19:16:07 +0900

    마크다운 에디터 작업완료

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import EditorTemplate from 'components/editor/EditorTemplate';
 import EditorHeader from 'components/editor/EditorHeader';
-import EditorPane from 'components/editor/EditorPane';
+import EditorPaneContainer from 'containers/editor/EditorPaneContainer';
+import PreviewPaneContainer from 'containers/editor/PreviewPaneContainer';
 
 const EditorPage = () => {
   return (
     <EditorTemplate
       header={<EditorHeader/>}
-      editor={<EditorPane/>}
-      preview="프리뷰"
+      editor={<EditorPaneContainer/>}
+      preview={<PreviewPaneContainer/>}
     />
   );
 };
