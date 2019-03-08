89793a5c74fb4312b828e165cca131210148509a
Seop <seungseop@users.noreply.github.com>
2019-03-08 14:03:11 +0900

    폴더 정리

--- src/lib/api.js
+++
@@ -1,4 +0,0 @@
-import axios from 'axios';
-
-export const writePost = ({title, body, tags}) => axios.post('/api/posts', {title, body, tags});
-export const getPost = (id) => axios.get(`/api/posts/${id}`);
